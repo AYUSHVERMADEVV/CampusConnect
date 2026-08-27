3b2f40e33593b46545fd5f19de212d735389c4f0
AYUSHVERMADEVV <av9918044987@gmail.com>
2026-08-27 15:29:13 +0530

    feat: add role-based authorization

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const { getMyProfile } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
-
+const { authorizeRoles } = require("../middleware/roleMiddleware");
 const router = express.Router();
 
 router.get("/profile", protect, getMyProfile);
-
+router.get(
+  "/admin-test",
+  protect,
+  authorizeRoles("admin"),
+  (req, res) => {
+    res.status(200).json({
+      message: "Welcome Admin! You have access.",
+      user: req.user,
+    });
+  }
+);
 module.exports = router;
